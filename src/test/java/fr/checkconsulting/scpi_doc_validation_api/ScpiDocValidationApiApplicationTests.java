1393d0a09e06c9a8e9cefd421d89004bec40f5ef
nasseraitahmed07 <aitahmednasser@outlook.com>
2025-11-08 11:09:37 +0100

    try to fix kafka test issue

--- src/test/java/fr/checkconsulting/scpi_doc_validation_api/ScpiDocValidationApiApplicationTests.java
+++ src/test/java/fr/checkconsulting/scpi_doc_validation_api/ScpiDocValidationApiApplicationTests.java
@@ -2,8 +2,10 @@ package fr.checkconsulting.scpi_doc_validation_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "spring.kafka.bootstrap-servers=dummy:9092")
 class ScpiDocValidationApiApplicationTests {
 
 	@Test

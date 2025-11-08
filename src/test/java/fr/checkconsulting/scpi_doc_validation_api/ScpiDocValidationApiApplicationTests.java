ad1f380a0479d789a50987e549769092ef6283b6
nasseraitahmed07 <aitahmednasser@outlook.com>
2025-11-08 11:19:57 +0100

    try to fix test isusue kafka 3

--- src/test/java/fr/checkconsulting/scpi_doc_validation_api/ScpiDocValidationApiApplicationTests.java
+++ src/test/java/fr/checkconsulting/scpi_doc_validation_api/ScpiDocValidationApiApplicationTests.java
@@ -2,8 +2,10 @@ package fr.checkconsulting.scpi_doc_validation_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ScpiDocValidationApiApplicationTests {
 
 	@Test

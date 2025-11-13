5aa9ee3a73a5a1739caf60893dde45c944cf22f0
Meziani Belkacem <belkacem.meziani@corepulse.dz>
2025-11-13 12:16:19 +0100

    add test for data initializer

--- src/main/java/fr/checkconsulting/scpi_doc_validation_api/config/RolePermissionDataInitializer.java
+++ src/main/java/fr/checkconsulting/scpi_doc_validation_api/config/RolePermissionDataInitializer.java
@@ -5,6 +5,7 @@ import fr.checkconsulting.scpi_doc_validation_api.repository.RolePermissionRepos
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -14,6 +15,7 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 @Slf4j
+@Profile("!test")
 public class RolePermissionDataInitializer implements CommandLineRunner {
 
     private final RolePermissionRepository repository;
